d299dd529ef82aaa43d14f33966fda8e0be619bd
Brendan Hauk <bhauk@SURGICALSCANNER.COM>
2017-08-01 09:16:07 -0400

    Test Commit

---
+++ login/login/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace login
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userName = new System.Windows.Forms.TextBox();
+            this.passWord = new System.Windows.Forms.TextBox();
+            this.loginBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // userName
+            // 
+            this.userName.Location = new System.Drawing.Point(37, 37);
+            this.userName.Name = "userName";
+            this.userName.Size = new System.Drawing.Size(173, 20);
+            this.userName.TabIndex = 0;
+            this.userName.Text = "Username";
+            // 
+            // passWord
+            // 
+            this.passWord.Location = new System.Drawing.Point(37, 77);
+            this.passWord.Name = "passWord";
+            this.passWord.PasswordChar = '*';
+            this.passWord.Size = new System.Drawing.Size(173, 20);
+            this.passWord.TabIndex = 1;
+            this.passWord.Text = "Password";
+            this.passWord.UseSystemPasswordChar = true;
+            // 
+            // loginBtn
+            // 
+            this.loginBtn.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.loginBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.loginBtn.Location = new System.Drawing.Point(65, 128);
+            this.loginBtn.Name = "loginBtn";
+            this.loginBtn.Size = new System.Drawing.Size(100, 31);
+            this.loginBtn.TabIndex = 2;
+            this.loginBtn.Text = "Login";
+            this.loginBtn.UseVisualStyleBackColor = false;
+            this.loginBtn.Click += new System.EventHandler(this.loginBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.loginBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(244, 207);
+            this.Controls.Add(this.loginBtn);
+            this.Controls.Add(this.passWord);
+            this.Controls.Add(this.userName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox userName;
+        private System.Windows.Forms.TextBox passWord;
+        private System.Windows.Forms.Button loginBtn;
+    }
+}
+
---
+++ login/login/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net;
+using System.DirectoryServices;
+using System.DirectoryServices.Protocols;
+using System.Security.Permissions;
+using System.Runtime.InteropServices;
+
+namespace login
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        [DirectoryServicesPermission(SecurityAction.LinkDemand, Unrestricted = true)]
+        private void loginBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                LdapDirectoryIdentifier ldi = new LdapDirectoryIdentifier("surgeserver", 389);//finds the server
+                System.DirectoryServices.Protocols.LdapConnection ldapConnection =
+                    new System.DirectoryServices.Protocols.LdapConnection(ldi);//connects to the server
+                Console.WriteLine("Connection made");
+                ldapConnection.AuthType = AuthType.Basic;
+                ldapConnection.SessionOptions.ProtocolVersion = 3;
+                NetworkCredential nc = new NetworkCredential(userName.Text,passWord.Text); 
+                ldapConnection.Bind(nc);//checks the username and password
+                Console.WriteLine("Conncetion succesful");
+                MessageBox.Show("Connection Succesful");
+                ldapConnection.Dispose();
+
+            }
+            catch (LdapException er) {
+                Console.WriteLine("Connection Error" + er.Message);
+                MessageBox.Show("Incorrect Credentials");
+            }
+
+
+
+
+        }
+        public class IncorrectException : Exception
+        {
+
+        }
+       
+    }
+}
